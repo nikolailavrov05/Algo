108881d28e79450414e6b1e3263e46a7c7460891
nikolailavrov05 <nikolai.lavrov05@gmail.com>
2026-03-23 21:07:26 +0300

    Delete stack_sort.py

--- stack_sort.py
+++
@@ -1,9 +0,0 @@
-def stack_sort_cherez_temp_stack(lst):
-    temporary = []
-    while lst:
-        current = lst.pop()
-        while temporary != Null and temporary[-1] < current:
-            lst.append(temporary.pop())
-        temporary.append(current)
-    while temporary:
-        lst.append(temporary.pop())
