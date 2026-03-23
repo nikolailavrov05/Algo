d11291fdeaa75fef201319acf977f280b6665882
nikolailavrov05 <nikolai.lavrov05@gmail.com>
2026-03-23 21:28:58 +0300

    Add files via upload

---
+++ stack_sort.py
@@ -0,0 +1,13 @@
+def stack_sort_cherez_temp_stack(start_stack):
+    if (start_stack == []):
+        return 0
+    temporary = []
+    while start_stack:
+        current = start_stack.pop()
+        while (temporary and temporary[-1] < current):
+            start_stack.append(temporary.pop())
+        temporary.append(current)
+    return temporary
+
+
+print(stack_sort_cherez_temp_stack([3, 4, 5]))
